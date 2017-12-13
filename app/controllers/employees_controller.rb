f5e5c4a137e330522ad32e629c60209ab68f083c
Adam Scroggin <arscroggin@sep.com>
2017-12-13 17:55:24 -0500

    Starting to show users

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,11 @@
 class EmployeesController < ApplicationController
+ #   before_action :authenticate_member!
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213193856) do
+ActiveRecord::Schema.define(version: 20171213223530) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
