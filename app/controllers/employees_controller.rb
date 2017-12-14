c0e1dcd20ff161377b546d0f65b532d3b3d13753
Adam Scroggin <arscroggin@sep.com>
2017-12-13 21:33:46 -0500

    devise is working. Login in page showing

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
- #   before_action :authenticate_member!
+    before_action :authenticate_user!
 
     def index
         @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'welcome/index'
- 
-  resources :employees
+  devise_for :user
+   
+  resources :employees, only: [:index, :show]
   end
