7ab6ee7b65e2d4e1a06bf310efaee1fc30c3fa58
Adam Scroggin <arscroggin@sep.com>
2017-12-18 14:30:08 -0500

    Saml Logger and mapping lastName to name

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  
+  def initialize
+    super # this calls ActionController::Base initialize
+    OneLogin::RubySaml::Logging.logger = Logger.new(File.open('./log/ruby-saml.log', 'w'))
+  end
+  
+  
   def index
     @user = current_user
   end
